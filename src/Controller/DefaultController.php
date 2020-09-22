a93bc694bc84ceabfc9c12bdcc2118889ea6d751
Aymruk <aymeric.painvin@gmail.com>
2020-09-22 15:22:03 +0200

    DefaultController

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\string;
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render("default/home.html.twig");
+    }
+}
