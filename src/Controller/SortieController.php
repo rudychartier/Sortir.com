b625112b0ef29b0b2dc896b05e9fc43908e3e25f
Aure-boop <aurechass@gmail.com>
2020-09-23 12:02:59 +0200

    changes

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,29 +2,87 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route ("/sortie", name="sortie_")
+ */
 
 class SortieController extends AbstractController
 {
+
     /**
-     * @Route("/sortie", name="sortie_list")
+     * @Route("/list", name="list")
      */
-    public function list()
+    public function list(EntityManagerInterface $em)
     {
-        // recup list en bdd
-        return $this->render('sortie/list.html.twig', [
-            'controller_name' => 'SortieController',
-        ]);
+        // recup sorties  en bdd
+        $sortieRepo = $em->getRepository(Sortie::class);
+        $sorties = $sortieRepo->find();
+        return $this->render('sortie/list.html.twig', ["sorties"=>$sorties]);
     }
 
     /**
-     * @Route("/sortie", name="sortie_add")
+     * @Route("/detail/{id}", name="detail", requirements={"id":"\d+"})
+     */
+    // methode seulement get à spécifier
+    public function detail($id,EntityManagerInterface $em,Request $request)
+    {
+        // recup detail sorties en bdd
+        $sortieRepo = $em->getRepository(Sortie::class);
+        $sorties =$sortieRepo->find($id);
+        return $this->render('sortie/detail.html.twig', ["sorties"=>$sorties]);
+    }
+
+    /**
+     * @Route("/add", name="add")
      */
     public function add()
     {
+        //créer la sortie , traiter + sauvergarder dans la bdd
+
         return $this->render('sortie/add.html.twig', [
             'controller_name' => 'SortieController',
         ]);
     }
+
+  /*  /**
+     * @Route("/publish", name="publish")
+     */
+   /* public function publish()
+    {
+        //publier la sortie
+        return $this->render('sortie/publish.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+*/
+    /**
+     * @Route("/delete", name="delete")
+     */
+    public function delete()
+    {
+        //supprimer la sortie
+        return $this->render('sortie/delete.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+
+    /**
+     * @Route("/cancel", name="cancel")
+     */
+    public function cancel ()
+    {
+        //annuler la sortie
+        return $this->render('sortie/cancel.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+
+
 }
